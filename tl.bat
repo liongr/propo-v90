34e05ee4f4cdab76a1daa41eea95129965a551e6
LEONIDAS PAPADOPOULOS <leonpap@gmail.com>
2020-12-19 19:16:00 +0200

    Update tl.bat

--- tl.bat
+++ tl.bat
@@ -1,4 +1,3 @@
-del v99.exe
-..\lang\tasm-5.0\bin\tasm *.asm>error1
-..\lang\tasm-5.0\bin\tlink /s @V99.RSP >errorl
-..\lang\tasm-5.0\bin\tasm /l sub0.asm
+tasm *.asm
+tlink /s @V99.RSP
+tasm /l sub0.asm
